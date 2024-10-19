64751f0b271c01198957cc00226e4c0850f9c62e
Tony Solomonik <tony.solomonik@gmail.com>
2024-10-19 14:32:06 +0300

    Add basic crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Result;
+use tracing::info;
+
+fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    info!("Hello, world!");
+
+    Ok(())
 }
