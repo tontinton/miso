795f44e680d9eb2919d5683599951887a61ba09f
Tony Solomonik <tony.solomonik@gmail.com>
2024-10-27 00:12:48 +0300

    query: Run vrl to filter logs

--- src/connector.rs
+++ src/connector.rs
@@ -1,3 +1,5 @@
+use std::pin::Pin;
+
 use futures_core::Stream;
 use serde::{Deserialize, Serialize};
 
@@ -10,9 +12,9 @@ pub enum Split {
 
 pub type Log = String;
 
-pub trait Connector {
+pub trait Connector: Send + Sync {
     fn get_splits(&self) -> Vec<Split>;
-    fn query(&self, split: &Split) -> Box<dyn Stream<Item = Log>>;
+    fn query(&self, split: &Split) -> Pin<Box<dyn Stream<Item = Log> + Send>>;
     fn apply_filter_and(&self, item: &FilterItem) -> bool;
     fn apply_filter_or(&self, items: &[FilterItem]) -> bool;
 }
--- src/http_server.rs
+++ src/http_server.rs
@@ -1,7 +1,17 @@
+use std::collections::BTreeMap;
+
 use axum::{http::StatusCode, routing::post, Json, Router};
+use color_eyre::eyre::bail;
+use color_eyre::Result;
+use futures_util::StreamExt;
 use serde::{Deserialize, Serialize};
 use tracing::info;
 use uuid::Uuid;
+use vrl::{
+    compiler::{compile, state::RuntimeState, Context, TargetValue, TimeZone},
+    diagnostic::{DiagnosticList, Severity},
+    value::{Secrets, Value},
+};
 
 use crate::ast::{ast_to_vrl, predicate_pushdown, QueryAst};
 use crate::{
@@ -27,6 +37,111 @@ struct Activity {
 #[derive(Debug)]
 struct Workflow {
     activities: Vec<Activity>,
+}
+
+fn severity_to_str(severity: &Severity) -> &'static str {
+    match severity {
+        Severity::Bug => "Bug",
+        Severity::Error => "Error",
+        Severity::Warning => "Warning",
+        Severity::Note => "Note",
+    }
+}
+
+fn pretty_print_diagnostics(diagnostics: &DiagnosticList) {
+    let message = diagnostics
+        .iter()
+        .map(|d| {
+            let labels = d
+                .labels
+                .iter()
+                .map(|l| {
+                    format!(
+                        "    [{}] {} (Span: {}-{})",
+                        if l.primary { "Primary" } else { "Secondary" },
+                        l.message,
+                        l.span.start(),
+                        l.span.end(),
+                    )
+                })
+                .collect::<Vec<_>>()
+                .join("\n");
+
+            let notes = d
+                .notes
+                .iter()
+                .map(|n| format!("    Note: {}", n))
+                .collect::<Vec<_>>()
+                .join("\n");
+
+            format!(
+                "{} [{}]: {}\n{}\n{}",
+                severity_to_str(&d.severity),
+                d.code,
+                d.message,
+                labels,
+                notes
+            )
+        })
+        .collect::<Vec<_>>()
+        .join("\n\n");
+    println!("{message}");
+}
+
+impl Workflow {
+    async fn execute(&self, connector: &dyn Connector) -> Result<()> {
+        // This is where each activity will be sent to a worker to be executed in parallel.
+        // Right now as an example, we run everything sequentially here.
+        for activity in &self.activities {
+            let post_program = if let Some(script) = &activity.post_script {
+                match compile(script, &vrl::stdlib::all()) {
+                    Ok(program) => {
+                        if !program.warnings.is_empty() {
+                            println!("Warnings:");
+                            pretty_print_diagnostics(&program.warnings);
+                        }
+                        Some(program.program)
+                    }
+                    Err(diagnostics) => {
+                        println!("Errors:");
+                        pretty_print_diagnostics(&diagnostics);
+                        bail!("Failed to compile VRL script:\n{}", script);
+                    }
+                }
+            } else {
+                None
+            };
+
+            match &activity.item {
+                ActivityItem::Scan(split) => {
+                    let mut query_stream = connector.query(split);
+                    while let Some(log) = query_stream.next().await {
+                        if let Some(ref program) = post_program {
+                            let mut target = TargetValue {
+                                value: serde_json::from_slice::<Value>(log.as_bytes())?,
+                                metadata: Value::Object(BTreeMap::new()),
+                                secrets: Secrets::default(),
+                            };
+                            let mut state = RuntimeState::default();
+                            let timezone = TimeZone::default();
+                            let mut ctx = Context::new(&mut target, &mut state, &timezone);
+                            let Value::Boolean(allowed) = program.resolve(&mut ctx)? else {
+                                bail!("Response of VRL script not boolean");
+                            };
+
+                            if !allowed {
+                                continue;
+                            }
+                        }
+
+                        println!("{log}");
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
 
 fn to_workflow(ast: &mut QueryAst, connector: &dyn Connector) -> Workflow {
@@ -71,7 +186,7 @@ async fn post_query_handler(
     info!(?req.query, "Starting to run a new query");
     let connector = ElasticsearchConnector {};
     let workflow = to_workflow(&mut req.query, &connector);
-    println!("{:?}", workflow);
+    workflow.execute(&connector).await.unwrap();
     (StatusCode::OK, Json(QueryResponse { query_id }))
 }
 
