ce20b0c7e1d1306285314cc1c86f01fd378a18ef
Tony Solomonik <tony.solomonik@gmail.com>
2024-11-02 16:46:39 +0200

    ast: Remove optimization of trying to predicate parts of AND

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-use crate::connector::Connector;
-
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "snake_case")]
 pub enum FilterAst {
@@ -9,37 +7,6 @@ pub enum FilterAst {
     And(Vec<FilterAst>),
     Term(/*field=*/ String, /*word=*/ String),
     Eq(/*field=*/ String, /*value=*/ String),
-}
-
-/// Tries to predicate pushdown the provided AST to the connector.
-/// Returns the (leftovers - stuff that the connector can't predicate, predicated).
-/// None means the expression was either kept as is (leftover) or taken fully (predicated).
-pub fn filter_predicate_pushdown(
-    ast: FilterAst,
-    connector: &dyn Connector,
-) -> (Option<FilterAst>, Option<FilterAst>) {
-    match ast {
-        FilterAst::And(exprs) => {
-            let mut leftovers = Vec::new();
-            let mut predicated = Vec::new();
-            for expr in exprs {
-                let (maybe_leftover, maybe_predicated) = filter_predicate_pushdown(expr, connector);
-                if let Some(expr) = maybe_leftover {
-                    leftovers.push(expr);
-                }
-                if let Some(expr) = maybe_predicated {
-                    predicated.push(expr);
-                }
-            }
-
-            (
-                (!leftovers.is_empty()).then_some(FilterAst::And(leftovers)),
-                (!predicated.is_empty()).then_some(FilterAst::And(predicated)),
-            )
-        }
-        _ if connector.can_filter(&ast) => (None, Some(ast)),
-        _ => (Some(ast), None),
-    }
 }
 
 fn binop_to_vrl(exprs: &[FilterAst], join: &str) -> String {
