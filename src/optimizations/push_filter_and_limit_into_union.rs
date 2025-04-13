19809e55547f6d05e409685534ab3c5c2db3b484
Tony Solomonik <tony.solomonik@gmail.com>
2025-04-13 16:29:45 +0300

    optimizations: don't keep filter after union before limit

--- src/optimizations/push_filter_and_limit_into_union.rs
+++ src/optimizations/push_filter_and_limit_into_union.rs
@@ -19,18 +19,21 @@ impl Optimization for PushFilterAndLimitIntoUnion {
         let (unions_start, unions_end) = groups[0];
         let (push_start, push_end) = groups[1];
 
+        let num_unions = unions_end - unions_start;
+
+        let union_steps = steps[unions_start..unions_end].to_vec();
         let push_steps = steps[push_start..push_end].to_vec();
+        let limit_step = steps[steps.len() - 1].clone();
 
-        let mut new_steps = Vec::with_capacity(push_steps.len() + steps.len());
+        let mut new_steps = Vec::with_capacity(push_steps.len() + num_unions + 1);
         new_steps.extend(push_steps.clone());
-        new_steps.extend(steps.to_vec());
+        new_steps.extend(union_steps);
+        new_steps.push(limit_step);
 
-        let union_range = push_steps.len() + unions_start..push_steps.len() + unions_end;
-        for step in &mut new_steps[union_range] {
-            let WorkflowStep::Union(ref mut workflow) = step else {
-                continue;
-            };
-            workflow.steps.extend(push_steps.clone());
+        for step in &mut new_steps[push_steps.len()..push_steps.len() + num_unions] {
+            if let WorkflowStep::Union(ref mut workflow) = step {
+                workflow.steps.extend(push_steps.clone());
+            }
         }
 
         Some(new_steps)
