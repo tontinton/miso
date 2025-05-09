ae806cc865292c2d85ed0763f776e2d213474a37
Tony Solomonik <tony.solomonik@gmail.com>
2025-05-09 17:04:12 +0300

    add graceful shutdown on Ctrl-C / SIGTERM

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
-use color_eyre::eyre::Result;
-use tokio::net::TcpListener;
+use color_eyre::eyre::{Context, Result};
+use tokio::{net::TcpListener, signal};
 use tracing::{debug, info};
 
 use miso::{args::parse_args, http_server::create_axum_app};
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+        info!("SIGINT/Ctrl+C received, starting graceful shutdown");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        use tokio::signal::unix::{signal, SignalKind};
+
+        // Kubernetes, Docker, systemd, etc...
+        let mut sigterm =
+            signal(SignalKind::terminate()).expect("failed to install SIGTERM handler");
+
+        sigterm.recv().await;
+        info!("SIGTERM received, starting graceful shutdown");
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -10,14 +39,16 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
     let args = parse_args();
-
     debug!(?args, "Init");
 
-    let listener = TcpListener::bind(&args.listen).await?;
     let app = create_axum_app(&args)?;
+    let listener = TcpListener::bind(&args.listen).await?;
 
     info!("Listening on {}", args.listen);
-    axum::serve(listener, app).await?;
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .context("axum serve")?;
 
     Ok(())
 }
