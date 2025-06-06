8ae156b53926c2815c3fe38e7f5bc14fa0596bdc
Tony Solomonik <tony.solomonik@gmail.com>
2025-06-06 17:01:45 +0300

    workflow: limit: remove log

--- src/workflow/limit.rs
+++ src/workflow/limit.rs
@@ -1,14 +1,10 @@
 use async_stream::try_stream;
-use futures_util::StreamExt;
-
 use color_eyre::Result;
-use tracing::info;
+use futures_util::StreamExt;
 
 use crate::log::{LogStream, LogTryStream};
 
 pub fn limit_stream(limit: u32, mut input_stream: LogStream) -> Result<LogTryStream> {
-    info!("Limitting to {limit}");
-
     Ok(Box::pin(try_stream! {
         let mut streamed = 0;
         while let Some(log) = input_stream.next().await {
