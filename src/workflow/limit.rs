adb81817cb69b8a4e460eb9189d4b799410f8033
Tony Solomonik <tony.solomonik@gmail.com>
2025-06-06 17:02:32 +0300

    workflow: limit: remove wrapping of Result
    Always `Ok`.

--- src/workflow/limit.rs
+++ src/workflow/limit.rs
@@ -1,11 +1,10 @@
 use async_stream::try_stream;
-use color_eyre::Result;
 use futures_util::StreamExt;
 
 use crate::log::{LogStream, LogTryStream};
 
-pub fn limit_stream(limit: u32, mut input_stream: LogStream) -> Result<LogTryStream> {
-    Ok(Box::pin(try_stream! {
+pub fn limit_stream(limit: u32, mut input_stream: LogStream) -> LogTryStream {
+    Box::pin(try_stream! {
         let mut streamed = 0;
         while let Some(log) = input_stream.next().await {
             if streamed >= limit {
@@ -14,5 +13,5 @@ pub fn limit_stream(limit: u32, mut input_stream: LogStream) -> Result<LogTryStr
             yield log;
             streamed += 1;
         }
-    }))
+    })
 }
