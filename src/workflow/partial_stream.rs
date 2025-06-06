fb9736df34528bfd4076d4b3ea92a0f342e64b41
Tony Solomonik <tony.solomonik@gmail.com>
2025-06-06 17:00:58 +0300

    workflow: top-n: support partial stream as input

--- src/workflow/partial_stream.rs
+++ src/workflow/partial_stream.rs
@@ -27,3 +27,20 @@ pub fn build_partial_stream_id_done_log(id: usize) -> Log {
     );
     log
 }
+
+pub fn get_partial_id(log: &Log) -> Option<(usize, bool)> {
+    let metadata = log.get(MISO_METADATA_FIELD_NAME)?;
+    let obj = metadata.as_object()?;
+
+    let id = obj
+        .get(PARTIAL_STREAM_ID_FIELD_NAME)
+        .and_then(|v| v.as_u64())
+        .map(|v| v as usize)?;
+
+    let done = obj
+        .get(PARTIAL_STREAM_DONE_FIELD_NAME)
+        .and_then(|v| v.as_bool())
+        .unwrap_or(false);
+
+    Some((id, done))
+}
