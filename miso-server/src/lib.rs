dc7a052b7b3c2d1ceb5a227db93a68dba367975c
Tony Solomonik <tony.solomonik@gmail.com>
2026-01-16 16:15:48 +0200

    write queries and their statuses to an updatable collection
    Currently only the memory connector support an updatable sink.
    
    In the future we might include postgres and iceberg for example.

--- miso-server/src/lib.rs
+++ miso-server/src/lib.rs
@@ -4,6 +4,7 @@ use miso_workflow_types::query::QueryStep;
 
 pub mod config;
 pub mod http_server;
+pub mod query_status;
 pub mod query_to_workflow;
 
 pub(crate) const VIEWS_CONNECTOR_NAME: &str = "views";
