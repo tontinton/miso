6d6d9471ca6b859a8a12a8d1007ec59d2cf896c7
Tony Solomonik <tony.solomonik@gmail.com>
2026-01-16 17:26:44 +0200

    args: move to doc comments instead of using help

--- src/args.rs
+++ src/args.rs
@@ -3,42 +3,28 @@ use clap::Parser;
 #[derive(Parser, Debug, Clone)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    #[clap(
-        short,
-        long,
-        help = "Path to JSON file with configurations (e.g. connectors to create on startup)."
-    )]
+    /// Path to JSON file with configurations (e.g. connectors to create on startup).
+    #[clap(short, long)]
     pub config: Option<String>,
 
-    #[clap(
-        short,
-        long,
-        help = "Listen address (host:port).",
-        default_value = "0.0.0.0:8080"
-    )]
+    /// Listen address (host:port).
+    #[clap(short, long, default_value = "0.0.0.0:8080")]
     pub listen: String,
 
-    #[clap(long, help = "Output logs formatted as JSON.", default_value = "false")]
+    /// Output logs formatted as JSON.
+    #[clap(long, default_value = "false")]
     pub log_json: bool,
 
-    #[clap(
-        long,
-        help = "Disable all optimizations (helpful for debugging purposes).",
-        default_value = "false"
-    )]
+    /// Disable all optimizations (helpful for debugging purposes).
+    #[clap(long, default_value = "false")]
     pub no_optimizations: bool,
 
-    #[clap(
-        long,
-        help = "Max distinct values of a field to be considered for dynamic filtering.",
-        default_value = "10000"
-    )]
+    /// Max distinct values of a field to be considered for dynamic filtering.
+    #[clap(long, default_value = "10000")]
     pub dynamic_filter_max_distinct_values: u64,
 
-    #[clap(
-        long,
-        help = "OTLP endpoint for exporting traces (e.g., http://localhost:4317)."
-    )]
+    /// OTLP endpoint for exporting traces (e.g., http://localhost:4317).
+    #[clap(long)]
     pub otlp_endpoint: Option<String>,
 }
 
